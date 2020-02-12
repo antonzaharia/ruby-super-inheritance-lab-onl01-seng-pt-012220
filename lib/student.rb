c5ff0bc72f68b5ad197e416511a0f9b5ca33ec52
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-12 16:43:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,9 @@
 class Student
   def hello
-    "Hi there! I'm so excited to learn stuff."
+    puts "Hi there! I'm so excited to learn stuff."
   end
   
   def raise_hand
-    "Pick me!"
+    puts "Pick me!"
   end
 end
