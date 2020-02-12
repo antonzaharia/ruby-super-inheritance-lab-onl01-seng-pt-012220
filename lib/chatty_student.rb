2e94af3456f00d74a95bfcab96c056236d7d1afa
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-12 16:48:19 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,6 +7,12 @@ class ChattyStudent < Student
   def raise_hand
     super
     puts "Pick me"
-    
+    puts "Pick me"
+    puts "Pick me"
+    puts "Pick me"
+    puts "Pick me"
+    puts "Pick me"
+    puts "Pick me"
+    puts "Pick me"
   end
 end
